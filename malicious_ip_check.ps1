aefb348d24ed59c287366dcfcb957bcd2209dd6a
matticamp <2312425+matticamp@users.noreply.github.com>
2018-04-28 19:31:52 -0400

    Add files via upload
    PowerShell script to check an IP address against a database of know malicious IP addresses.

---
+++ malicious_ip_check.ps1
@@ -0,0 +1,14 @@
+﻿$WebResponse = Invoke-WebRequest "https://lists.blocklist.de/lists/all.txt"
+$output=Read-Host -Prompt "Enter your output file path: "
+$WebResponse.Content |Out-File $output
+
+$array=Get-Content $output
+
+$ip=Read-Host -Prompt "Enter an IP address: "
+
+if ($array -contains $ip){
+'Malicious'
+} Else {
+'No hit found'
+}
+
